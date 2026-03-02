d4d4c91e1682e6846078f1c84e97bf151698ea00
opeyemiTaiwo <159091973+opeyemiTaiwo@users.noreply.github.com>
2026-03-02 15:21:01 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// src/App.jsx - update
+// src/App.jsx
 
 import React, { Suspense, lazy, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
